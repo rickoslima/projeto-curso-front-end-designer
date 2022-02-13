a8eefe8864e614ba3918f880289eea976d69c0bb
Ricardo Lima <71652559+rickoslima@users.noreply.github.com>
2022-02-13 18:35:59 -0300

    20220213
    page home

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,5 +1,19 @@
+//Declarando Variáveis
+
 var btnContac = document.querySelector('.jl-btn-contact');
 
+//Page Preloader
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.jl-preloader');
+    pagePreloder.classList.add('jl-fade-out');
+     
+    setTimeout(function () {
+    pagePreloder.style.display = 'none';
+    }, 2000);
+    });
+
+
+//Abrindo e fechando informações de contato
 btnContac.addEventListener('click', function(){
     var boxContact = document.querySelector('.jl-contact-info');
 
@@ -7,3 +21,4 @@ btnContac.addEventListener('click', function(){
 
     this.classList.toggle('jl-change-icon');
 });
+
