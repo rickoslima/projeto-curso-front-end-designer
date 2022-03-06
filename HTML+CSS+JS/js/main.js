907e6d86b87de13b14517f7b0898f57da973a72e
Ricardo Lima <71652559+rickoslima@users.noreply.github.com>
2022-03-06 16:29:33 -0300

    inclusão Scroll

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,32 +1,29 @@
 //Declarando Variáveis
-
-var btnContac = document.querySelector('.jl-btn-contact');
-
+var btnContact = document.querySelector('.jl-btn-contact');
 var toggleModal = document.querySelectorAll('.jl-toggle-modal');
 
 //Page Preloader
 window.addEventListener('load', function () {
     var pagePreloder = document.querySelector('.jl-preloader');
     pagePreloder.classList.add('jl-fade-out');
-     
+
     setTimeout(function () {
-    pagePreloder.style.display = 'none';
+        pagePreloder.style.display = 'none';
     }, 2000);
-    });
+});
 
 
-//Abrindo e fechando informações de contato
-btnContac.addEventListener('click', function(){
+//Abrindo e Fechando Informações de Contato
+btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
-
     boxContact.classList.toggle('jl-is-open');
-
     this.classList.toggle('jl-change-icon');
 });
 
-//Abrindo e Fechando o modal de orçamento
-for(var i = 0; i < toggleModal.length; i++){
-    toggleModal[i].addEventListener('click', function(){
+//Abrindo e Fechando o Modal de Orcamento
+
+for (var i = 0; i < toggleModal.length; i++) {
+    toggleModal[i].addEventListener('click', function () {
         var overlay = document.querySelector('.jl-overlay');
         var modalOrcamento = document.querySelector('#jl-modal-orcamento');
 
@@ -37,13 +34,69 @@ for(var i = 0; i < toggleModal.length; i++){
     });
 }
 
-//Animando elementos on scroll com Waypoints
+// Animando Elementos on Scroll com Waypoints
 var myScrollDown = document.querySelector('.jl-scroll-down');
-
 var waypoint = new Waypoint({
     element: myScrollDown,
-    handler: function() {
-      myScrollDown.classList.toggle('jl-fade-out');
+    handler: function () {
+        myScrollDown.classList.toggle('jl-fade-out');
     },
-    offset: '50%'
-  })
+    offset: '80%'
+});
+
+//PORTFOLIO SLIDER
+
+//Declarando variáveis do slider
+var sliderContainer = document.querySelector('.jl-slider-container');
+var sliderList = document.querySelector('.jl-slider-list');
+var sliderItem = document.querySelectorAll('.jl-slider-item');
+var sliderListWidth = null;
+
+//Capturando larguras individuais
+var containerWidth = sliderContainer.parentElement.offsetWidth;
+
+//Passando larguras dinâmicas
+sliderContainer.style.width = containerWidth + 'px';
+
+for (var p = 0; p < sliderItem.length; p++) {
+    sliderItem[p].style.width = containerWidth + 'px';
+    var sliderItemWidth = sliderItem[p].offsetWidth;
+
+    sliderListWidth += sliderItemWidth;
+}
+
+sliderList.style.width = sliderListWidth + 'px';
+
+
+//Fazendo Animaçao do Slider onClick
+
+var prevItem = document.querySelector('.jl-item-prev');
+var nextItem = document.querySelector('.jl-item-next');
+var sliderPos = 0;
+
+nextItem.addEventListener('click', function () {
+    var lastItem = sliderListWidth - containerWidth;
+
+    if ((-1 * (sliderPos) === lastItem)) {
+        return;
+    }
+
+    sliderPos -= containerWidth;
+    anime({
+        targets: sliderList,
+        translateX: sliderPos
+    });
+});
+
+prevItem.addEventListener('click', function () {
+
+    if (sliderPos === 0) {
+        return;
+    }
+
+    sliderPos += containerWidth;
+    anime({
+        targets: sliderList,
+        translateX: sliderPos
+    });
+});
